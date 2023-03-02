a8fb567b6849c72caf9569449e6f40f5e475f2b4
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-03-02 17:25:54 +0530

    use module

--- main.tf
+++ main.tf
@@ -14,9 +14,27 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
 
   tags = {
     Name = "HelloWorld"
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_ami" {
-  value = aws_instance.web.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.blog.arn
 }
 
 
