3db47b1c577c111bdd01bd3c6659c5e0959d590a
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-03-16 11:23:39 +0530

    auto scaling

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.id
+  vpc_id  = module.blog_vpc.public_subnets[0]
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -97,4 +97,19 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  # insert the 1 required variable here
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+  image_id = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
 }
