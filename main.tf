ef869d4dd8addc126e67b132f864880280da09e3
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-03-16 11:14:28 +0530

    fake run

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
